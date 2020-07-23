aa4ed3ef9b73ab9d17795fffaf475f9026b94153
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 23:06:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
-  attr_accessor :name, :birthday, :height, :complexion
-  
-  def initialize(name:, birthday:, height: complexion:)
-    
-  end
-  
-end
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="),value)}
+  end 
+end 
